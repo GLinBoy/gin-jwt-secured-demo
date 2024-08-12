014a250174e09c28d59b032057cf450e3de4879a
GLinBoy <glinboy@live.com>
2024-08-12 19:38:03 +0200

    👌 IMPROVE: Added save method impl. to repository

--- repository/tags_repository_impl.go
+++ repository/tags_repository_impl.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/glinboy/gin-jwt-secured-demo/model"
+	"gorm.io/gorm"
+)
 
 type TagsRepositoryImpl struct {
 	Db *gorm.DB
@@ -8,4 +11,8 @@ type TagsRepositoryImpl struct {
 
 func NewTagsRepositoryImpl(Db *gorm.DB) TagsRepository {
 	return &TagsRepositoryImpl{Db: Db}
+}
+
+func (t TagsRepositoryImpl) Save(tags model.Tags) {
+	t.Db.Create(&tags)
 }
