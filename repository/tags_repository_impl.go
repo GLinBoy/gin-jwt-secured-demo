f8c041a7a4f9f966e38d4c512863e5fd196f38df
GLinBoy <glinboy@live.com>
2024-08-12 19:44:53 +0200

    👌 IMPROVE: Added FindById impl. to repository impl.

--- repository/tags_repository_impl.go
+++ repository/tags_repository_impl.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/glinboy/gin-jwt-secured-demo/model"
 	"gorm.io/gorm"
 )
@@ -24,4 +26,14 @@ func (t TagsRepositoryImpl) Update(tags model.Tags) {
 func (t TagsRepositoryImpl) Delete(tagsId int) {
 	var tags model.Tags
 	t.Db.Where("id = ?", tagsId).Delete(&tags)
+}
+
+func (t TagsRepositoryImpl) FindById(tagsId int) (model.Tags, error) {
+	var tag model.Tags
+	result := t.Db.Find(&tag, tagsId)
+	if result != nil {
+		return tag, nil
+	} else {
+		return tag, errors.New("tag is not found")
+	}
 }
