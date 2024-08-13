1a6a3a2c3da78320bf4dff12e324cdc7f3c18d50
GLinBoy <glinboy@live.com>
2024-08-13 20:55:09 +0200

    👌 IMPROVE: Added constructor for service impl

--- service/tags_service_impl.go
+++ service/tags_service_impl.go
@@ -9,3 +9,10 @@ type TagsServiceImpl struct {
 	TagsRepository repository.TagsRepository
 	Validate       *validator.Validate
 }
+
+func NewTagServiceImpl(tagRepository repository.TagsRepository, valitate *validator.Validate) TagsService {
+	return &TagsServiceImpl{
+		TagsRepository: tagRepository,
+		Validate:       valitate,
+	}
+}
