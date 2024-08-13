2dd260d75f6ad0153b023dbf59930c4456734e9c
GLinBoy <glinboy@live.com>
2024-08-13 20:51:11 +0200

    👌 IMPROVE: Updated service impl. constructor

--- service/tags_service_impl.go
+++ service/tags_service_impl.go
@@ -1,4 +1,11 @@
 package service
 
+import (
+	"github.com/glinboy/gin-jwt-secured-demo/repository"
+	"github.com/go-playground/validator/v10"
+)
+
 type TagsServiceImpl struct {
+	TagsRepository repository.TagsRepository
+	Validate       *validator.Validate
 }
