105b8e6dcd249a7d3a133bc6b64f5d079625bc18
GLinBoy <glinboy@live.com>
2024-08-14 20:07:05 +0200

    👌 IMPROVE: Added constructor method to tag controller

--- controller/tag_controller.go
+++ controller/tag_controller.go
@@ -5,3 +5,7 @@ import "github.com/glinboy/gin-jwt-secured-demo/service"
 type TagController struct {
 	tagService service.TagsService
 }
+
+func NewTagController(service service.TagsService) *TagController {
+	return &TagController{tagService: service}
+}
