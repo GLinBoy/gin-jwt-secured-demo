5f9f8445d9653e63cf020fe07b247dff6016c277
GLinBoy <glinboy@live.com>
2024-08-14 20:12:25 +0200

    👌 IMPROVE: Added create method to tag controller

--- controller/tag_controller.go
+++ controller/tag_controller.go
@@ -1,6 +1,13 @@
 package controller
 
-import "github.com/glinboy/gin-jwt-secured-demo/service"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/glinboy/gin-jwt-secured-demo/data/request"
+	"github.com/glinboy/gin-jwt-secured-demo/data/response"
+	"github.com/glinboy/gin-jwt-secured-demo/service"
+)
 
 type TagController struct {
 	tagService service.TagsService
@@ -8,4 +15,20 @@ type TagController struct {
 
 func NewTagController(service service.TagsService) *TagController {
 	return &TagController{tagService: service}
+}
+
+func (controller *TagController) Create(ctx *gin.Context) {
+	createTagRequest := request.CreateTagsRequest{}
+	err := ctx.ShouldBindJSON(&createTagRequest)
+	if err != nil {
+		panic(err)
+	}
+	controller.tagService.Create(createTagRequest)
+
+	webResponse := response.Response{
+		Code:   200,
+		Status: "Ok",
+		Data:   nil,
+	}
+	ctx.JSON(http.StatusOK, webResponse)
 }
