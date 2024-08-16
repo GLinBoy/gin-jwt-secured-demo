6898407fd14b24e956c75462db542daf24bec15e
GLinBoy <glinboy@live.com>
2024-08-16 20:31:34 +0200

    👌 IMPROVE: Added home page router

--- router/router.go
+++ router/router.go
@@ -7,6 +7,21 @@ import (
 
 func NewRouter(tagController *controller.TagController) *gin.Engine {
 	service := gin.Default()
+service.GET("", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "Welcome Home!")
+	})
+
+	service.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	})
+
+	router := service.Group("/api")
+	tagRouter := router.Group("/tag")
+	tagRouter.GET("", tagController.FindAll)
+	tagRouter.GET("/:tagId", tagController.FindById)
+	tagRouter.POST("", tagController.Create)
+	tagRouter.PATCH("/:tagId", tagController.Update)
+	tagRouter.DELETE("/:tagId", tagController.Delete)
 
 	return service
 }
