ea532852fecd90c16da4caa8bf3d416022edfb84
GLinBoy <glinboy@live.com>
2024-08-17 12:53:11 +0200

    👌 IMPROVE: Added basic methods of user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,4 +1,10 @@
 package repository
 
+import "github.com/glinboy/gin-jwt-secured-demo/model"
+
 type UserRepository interface {
+	Save(user model.User)
+	Update(user model.User)
+	FindById(userId int) model.User
+	FindByEmail(email string) model.User
 }
