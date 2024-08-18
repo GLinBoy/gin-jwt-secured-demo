4aa199ad48b4f13b8937fcf7559dfa25a1c9456b
GLinBoy <glinboy@live.com>
2024-08-18 13:19:21 +0200

    🐛 FIX: Misspell

--- service/tags_service_impl.go
+++ service/tags_service_impl.go
@@ -13,10 +13,10 @@ type TagsServiceImpl struct {
 	Validate       *validator.Validate
 }
 
-func NewTagServiceImpl(tagRepository repository.TagsRepository, valitate *validator.Validate) TagsService {
+func NewTagServiceImpl(tagRepository repository.TagsRepository, validate *validator.Validate) TagsService {
 	return &TagsServiceImpl{
 		TagsRepository: tagRepository,
-		Validate:       valitate,
+		Validate:       validate,
 	}
 }
 
