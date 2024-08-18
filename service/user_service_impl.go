a97379f8132c09c080670bdbff4a4b39109e2336
GLinBoy <glinboy@live.com>
2024-08-18 13:35:54 +0200

    👌 IMPROVE: Added signup  method to user service impl.

--- service/user_service_impl.go
+++ service/user_service_impl.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"github.com/glinboy/gin-jwt-secured-demo/data/request"
+	"github.com/glinboy/gin-jwt-secured-demo/model"
 	"github.com/glinboy/gin-jwt-secured-demo/repository"
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserServiceImpl struct {
@@ -15,4 +18,21 @@ func NewUserServiceImpl(userRepository repository.UserRepository, validate *vali
 		UserRepository: userRepository,
 		Validate:       validate,
 	}
+}
+
+func (u UserServiceImpl) Signup(user request.CreateUserRequest) {
+	err := u.Validate.Struct(user)
+	if err != nil {
+		panic(err)
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		panic(err)
+	}
+	userModel := model.User{
+		FullName: user.FullName,
+		Email:    user.Email,
+		Password: string(hash),
+	}
+	u.UserRepository.Save(userModel)
 }
