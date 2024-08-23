7fa1bad5d9bd43ba512e270c676705b726eb6654
GLinBoy <glinboy@live.com>
2024-08-23 19:29:44 +0200

    👌 IMPROVE: Added user controller constructor

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -5,3 +5,7 @@ import "github.com/glinboy/gin-jwt-secured-demo/service"
 type UserController struct {
 	userService service.UserService
 }
+
+func NewUserController(service service.UserService) *UserController {
+	return &UserController{userService: service}
+}
