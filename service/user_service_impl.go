3059377895a3527c8512ea80df86ccab6b363f6f
GLinBoy <glinboy@live.com>
2024-08-23 19:21:14 +0200

    👌 IMPROVE: Added signin method impl to user service

--- service/user_service_impl.go
+++ service/user_service_impl.go
@@ -1,10 +1,14 @@
 package service
 
 import (
+	"time"
+
 	"github.com/glinboy/gin-jwt-secured-demo/data/request"
+	"github.com/glinboy/gin-jwt-secured-demo/data/response"
 	"github.com/glinboy/gin-jwt-secured-demo/model"
 	"github.com/glinboy/gin-jwt-secured-demo/repository"
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -35,4 +39,32 @@ func (u UserServiceImpl) Signup(user request.CreateUserRequest) {
 		Password: string(hash),
 	}
 	u.UserRepository.Save(userModel)
+}
+
+func (u UserServiceImpl) Signin(signin request.SigninRequest) response.TokenResponse {
+	userData, err := u.UserRepository.FindByEmail(signin.Email)
+	if err != nil {
+		panic(err)
+	}
+	if bcrypt.CompareHashAndPassword([]byte(userData.Password), []byte(signin.Password)) != nil {
+		panic("Invalid email or password")
+	}
+
+	expiration := time.Now().Add(time.Hour * 24 * 30).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": userData.Email,
+		"exp": expiration,
+	})
+
+	tokenString, err := token.SignedString([]byte("SECURE_SECRET"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return response.TokenResponse{
+		Token:      tokenString,
+		Expiration: expiration,
+	}
 }
