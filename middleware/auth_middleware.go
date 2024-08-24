e859b542d547996ec9a5cef79fb8173ff0af8d45
GLinBoy <glinboy@live.com>
2024-08-24 13:29:37 +0200

    👌 IMPROVE: Add token verification impl.

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,5 +1,41 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
-func RequireAuth(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/glinboy/gin-jwt-secured-demo/config"
+	"github.com/glinboy/gin-jwt-secured-demo/repository"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func RequireAuth(ctx *gin.Context) {
+	tokenString, err := ctx.Cookie("Authorization")
+	if err != nil {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error){
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("SECURE_SECRET"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		user, err := repository.NewUserRepositoryImpl(config.DatabaseConnection()).FindByEmail(claims["sub"].(string))
+		if err != nil && user.Id == 0 {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		ctx.Set("user", user)
+
+		ctx.Next()
+	}
+}
